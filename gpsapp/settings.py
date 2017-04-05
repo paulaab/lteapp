86d99286e75f1a44323c17260b5cb28ee93f802c
paulaab <paulaa_bohorquez@hotmail.com>
2017-04-05 16:15:16 +0200

    First map with markers assigned

--- gpsapp/settings.py
+++ gpsapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!7hvpqshd--+w%@ct*xq_3h-ay66-7i+67etv!-*59y#zcn&77'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['innogarage.pythonanywhere.com']
+ALLOWED_HOSTS = ['innogarage.pythonanywhere.com', 'localhost']
 
 
 # Application definition
